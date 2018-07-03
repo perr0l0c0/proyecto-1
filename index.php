24702ef6d93b1436cad47f7cdf19772e5d8283e6
Cristian <cristiancastroalarcon@gmail.com>
2018-07-03 18:21:04 +0200

    bootstrap + jquery

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title></title>
+  </head>
+  <body>
+    <div class="col-offset-2 col-md-6">
+      asdf
+    </div>
+  </body>
+</html>
+
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/jquery.js"></script>
